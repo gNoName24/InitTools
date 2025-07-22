087aabe26575e686b8fce448233cbab2df4265be
gNoName24 <146374556+gNoName24@users.noreply.github.com>
2025-07-23 00:42:31 +0500

    Init

---
+++ src/InitLogger.cpp
@@ -0,0 +1,14 @@
+#include "InitLogger/InitLogger.h"
+
+std::shared_ptr<spdlog::logger> Logger = spdlog::stdout_color_mt("console");
+
+void initLogger() {
+    auto formatter = std::make_unique<spdlog::pattern_formatter>();
+
+    formatter->add_flag<LevelShortForm>('S');
+    formatter->add_flag<AlignedLocationFlag>('L');
+
+    formatter->set_pattern("[%^%S%$] [%T] [%L] %v");
+
+    Logger->set_formatter(std::move(formatter));
+}
