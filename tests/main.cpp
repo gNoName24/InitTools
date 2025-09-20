b3e8c4040b60c609e2ce4174b1332a4ea58a9a94
NoName24 <146374556+gNoName24@users.noreply.github.com>
2025-09-20 18:50:41 +0500

    Исправление версии в CMakeLists.txt

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,66 +1,36 @@
-//#include <chrono>
-#include <iostream>
 #include <InitTools/InitConsole.hpp>
-#include <InitTools/InitLocale.hpp>
 #include <InitTools/InitWindow.hpp>
+#include <InitTools/InitGraphics.hpp>
 
 // Логгер
-InitConsole::Logger logger("test logger");
+InitConsole::Logger logger("logger");
 
-// Менеджер локали
-InitLocale::LocaleManager locale_manager;
+// Окно
+InitWindow::WindowManager* window = nullptr;
+
+// InitGraphics
+InitGraphics::BaseCVVE CVVE;
 
 int main() {
-    /*using clock = std::chrono::high_resolution_clock;
-    auto start = clock::now();*/
-
-    //for(int i = 0; i < 1000000; i++) {
-    //    logger.log_info("for " + std::to_string(i));
-    //}
-
-    /*auto end = clock::now();
-    std::chrono::duration<double, std::milli> ms = end - start;
-    logger.log_info("Время выполнения: " + std::to_string(ms.count()) + " ms");*/
-
-    /*InitLocale::extern_function_log = [](const std::string& msg, const std::string& level, const std::string& file_name, int line, const std::string& function_name) {
-        logger.log(msg, level, file_name, line, function_name);
-    };
-
-    logger.log_info("get_locale_current_system: " + InitLocale::get_locale_current_system());
-    logger.log_info("get_locale_language: " + InitLocale::get_locale_language(InitLocale::get_locale_current_system()));
-    logger.log_info("get_locale_territory: " + InitLocale::get_locale_territory(InitLocale::get_locale_current_system()));
-    logger.log_info("get_locale_language_territory: " + InitLocale::get_locale_language_territory(InitLocale::get_locale_current_system()));
-    logger.log_info("get_locale_codeset: " + InitLocale::get_locale_codeset(InitLocale::get_locale_current_system()));
-
-    locale_manager.root = std::filesystem::current_path() / "resources" / "locale";
-    locale_manager.domain = "test";
-    locale_manager.primary = InitLocale::get_locale_language(InitLocale::get_locale_current_system());
-    locale_manager.fallback = "en";
-    locale_manager.build();
-    logger.log_info(locale_manager.gettext("test.key"));*/
-
-    // Окно
     InitWindow::extern_function_log = [](const std::string& msg, const std::string& level, const std::string& file_name, int line, const std::string& function_name) {
         logger.log(msg, level, file_name, line, function_name);
     };
-
-    InitWindow::WindowManager* window = nullptr;
-    InitWindow::window_new("test_window", &window);
+    InitWindow::window_new("window", &window);
     window->initialization();
+    window->set_fps_target(60);
 
-    int time_int_back = 0;
-    window->render_start([&window, &time_int_back]() {
-        glClear(GL_COLOR_BUFFER_BIT);
+    CVVE.initialization();
+
+    window->render_start(
+    []() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        int time_int = static_cast<int>(window->get_time());
-        if(time_int != time_int_back) {
-            window->set_title("FPS: " + std::to_string(window->get_fps()));
-            time_int_back = time_int;
-        }
+        glClear(GL_COLOR_BUFFER_BIT);
     });
-    window->while_start();
-    window->render_stop();
 
+    window->while_start();
+
+    window->render_stop();
+    CVVE.terminate();
     window->terminate();
     return 0;
 }
